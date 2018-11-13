e5c83c13df9ed57da59f67c732f490f607fd27c0
loth <eljugador360@gmail.com>
2018-11-12 22:21:59 -0600

    login basico

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 
 router.get('/', (req, res) => {
-    res.render('index.html', { title: 'AdHoc ' })
+    res.render('index.ejs', { title: 'AdHoc ' })
    
  });
 
@@ -12,5 +12,47 @@ router.get('/', (req, res) => {
    
  });
 
+ router.get('/signup',isAuthenticated, (req, res, next) => {
+    res.render('signup');
+  });
+  
+  router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+  })); 
+  
+  router.get('/signin', (req, res, next) => {
+    res.render('signin');
+  });
+  
+  
+  router.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    failureFlash: true
+  }));
+  
+  router.get('/profile',isAuthenticated, (req, res, next) => {
+    res.render('profile')
+    
+  });
+  
+  router.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/');
+  });
+  
+  
+  function isAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+      return next();
+    }
+  
+    res.redirect('/signin')
+  }
 
- module.exports = router;
+  
+  
+  
+  module.exports = router;
